1a44cb89e7fa07712ba066c3ab53090e609d60cf
Craxa <48805031+Craxa@users.noreply.github.com>
2022-05-02 19:45:26 +0100

    Update FemWare.lua

--- FemWare.lua
+++ FemWare.lua
@@ -2,5 +2,5 @@ if game.PlaceId == 7305309231 then
 	loadstring(game:HttpGet("https://api.upload.systems/pastes/I0AOyvfnLt0V/raw",true))()
     end
 if game.PlaceId == 4588604953 then
-	loadstring(game:HttpGet("https://api.upload.systems/pastes/GiIPvgT5yyq5/raw",true))()
+	loadstring(game:HttpGet("https://api.upload.systems/pastes/9JpNPrbJJHZZ/raw",true))()
     end
