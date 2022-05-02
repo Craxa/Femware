219528ae6fce2eb50823a96167d2c8be45119a01
Craxa <48805031+Craxa@users.noreply.github.com>
2022-05-02 19:35:23 +0100

    Update FemWare.lua

--- FemWare.lua
+++ FemWare.lua
@@ -1,2 +1,6 @@
-_G.FemKess = "FemWare On Top"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JackHiggly/RobloxThings/main/FemWare0", true))()
+if game.PlaceId == 7305309231 then
+	loadstring(game:HttpGet("https://api.upload.systems/pastes/I0AOyvfnLt0V/raw",true))()
+    end
+if game.PlaceId == 4588604953 then
+	loadstring(game:HttpGet("https://api.upload.systems/pastes/GiIPvgT5yyq5/raw",true))()
+    end
