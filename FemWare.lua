b4c98b1063ae2520c4826834b7a429a907c27230
Craxa <48805031+Craxa@users.noreply.github.com>
2022-06-26 00:21:52 +0100

    Update and rename FemWare.lua to UI.lua

--- FemWare.lua
+++
@@ -1,6 +0,0 @@
-if game.PlaceId == 7305309231 then
-	loadstring(game:HttpGet("https://api.upload.systems/pastes/I0AOyvfnLt0V/raw",true))()
-    end
-if game.PlaceId == 4588604953 then
-	loadstring(game:HttpGet("https://api.upload.systems/pastes/5XvNiUBwvXpi/raw",true))()
-    end
