3b7d39c9d45ced8768c433c50f0a06055d00512d
Craxa <48805031+Craxa@users.noreply.github.com>
2022-06-26 01:20:29 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -46,8 +46,8 @@ library.theme = {
 
 if not _G.AccentColors then
     _G.AccentColors = {
-        Accent1 = Color3.fromRGB(252, 255, 255),
-        Accent2 = Color3.fromRGB(255, 255, 255)
+        Accent1 = Color3.fromRGB(69, 22, 121),
+        Accent2 = Color3.fromRGB(33, 52, 150)
     }
 end
 
